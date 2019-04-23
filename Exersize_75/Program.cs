971d8f10be1edc6ebac1df1beee80d90a403bd07
Maddy Haldeman <madelyn.haldeman@gmail.com>
2019-04-23 14:39:02 -0700

    complete exersize 73

---
+++ Exersize_75/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exersize_75
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            string strAge = Console.ReadLine();
+            int age = Convert.ToInt32(strAge);
+            //Console.WriteLine(age + "\n");
+
+            Console.WriteLine("Have you ever had a DUI? (true/false)");
+            string strDui = Console.ReadLine();
+            bool dui = Convert.ToBoolean(strDui);
+            //Console.WriteLine(dui + "\n");
+
+            Console.WriteLine("How many tickets do you have?");
+            string strTicket = Console.ReadLine();
+            int ticket = Convert.ToInt32(strTicket);
+            //Console.WriteLine(ticket + "\n");
+
+            Console.WriteLine("Qualified?");
+            bool qualified = (age > 15 && dui == false && ticket <= 3);
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+        }
+    }
+}

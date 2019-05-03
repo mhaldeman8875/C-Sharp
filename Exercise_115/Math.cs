b1b55dcc84370a6194d6c358586994550c69e5ab
Maddy Haldeman <madelyn.haldeman@gmail.com>
2019-05-03 15:14:16 -0700

    115 - optional parameters

---
+++ Exercise_115/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_115
+{
+    class Math
+    {
+        public int operation(int required, int optional = 10)
+        {
+            int result = required / optional;
+            return result;
+        }
+    }
+}
---
+++ Exercise_115/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.Write("Enter a number: ");
+            int required = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter another number OR hit the enter key to skip this step: ");
+            string input = Console.ReadLine();
+            int optional;
+
+            if (int.TryParse(input, out optional))
+            {
+                optional = Convert.ToInt32(input);
+                int returns = math.operation(required, optional);
+                Console.WriteLine(required + " / " + optional + " = " + returns);
+            }
+            else
+            {
+                int returns = math.operation(required);
+                Console.WriteLine(required + " / 10 = " + returns);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

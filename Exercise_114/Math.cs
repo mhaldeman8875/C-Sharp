c181148a61f8c7ea1472e6094b3be5f7d967b542
Maddy Haldeman <madelyn.haldeman@gmail.com>
2019-05-03 14:42:51 -0700

    even more methods

---
+++ Exercise_114/Math.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_114
+{
+    class Math
+    {
+        //  method that takes an integer and finds out if it is even or odd
+        public int evenOdd (int input)
+        {
+            int result = input % 2;
+
+            //if (result == 0)
+            //{
+            //    Console.WriteLine("This number is even");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("This number is odd");
+            //}
+            return result;
+            
+        }
+
+        public int evenOdd (int input, decimal dec)
+        {
+            decimal result = input + dec;
+            int resultInt = Decimal.ToInt32(result);
+            return resultInt;
+        }
+
+        public int evenOdd (string str)
+        {
+            int resultInt = Convert.ToInt32(str);
+            int result = resultInt / 4;
+            return result;
+        }
+    }
+}
---
+++ Exercise_114/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            Console.Write("Enter a whole number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int returns = math.evenOdd(input);
+            if (returns == 0)
+            {
+                Console.WriteLine(input + " is an even number");
+            }
+            else
+            {
+                Console.WriteLine(input + " is an odd number");
+            }
+
+            Console.Write("\nEnter a decimal: ");
+            decimal dec = Convert.ToDecimal(Console.ReadLine());
+            int returns2 = math.evenOdd(input, dec);
+            if (returns2 % 2 == 0)
+            {
+                Console.WriteLine(input + " + " + dec + " = " + returns2 + " , which is an even number");
+            }
+            else
+            {
+                Console.WriteLine(input + " + " + dec + " = " + returns2 + " , which is an odd number");
+            }
+
+            Console.Write("\nFinally, enter one last number: ");
+            string str = Console.ReadLine();
+            int returns3 = math.evenOdd(str);
+            if (returns3 % 2 == 0)
+            {
+                Console.WriteLine(str + " / 4 = " + returns3 + " , which is an even number");
+            }
+            else
+            {
+                Console.WriteLine(str + " / 4 = " + returns3 + " , which is an odd number");
+            }
+
+            Console.ReadLine();
+
+        }
+
+    }
+}

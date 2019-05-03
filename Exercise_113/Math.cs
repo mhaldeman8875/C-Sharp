f5a5334c651c3d8103c134e73c63e30164536712
Maddy Haldeman <madelyn.haldeman@gmail.com>
2019-05-03 13:01:36 -0700

    more methods

---
+++ Exercise_113/Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_113
+{
+    class Math
+    {
+        public void integers(int first, int second)
+        {
+            int result = first + 30 / 2;
+            Console.WriteLine("\n" + first + " + 30 / 2 = " + result);
+            Console.WriteLine("\nHere's your second input: " + second);
+        }
+    }
+}
---
+++ Exercise_113/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate Math class
+            Math Math = new Math();
+
+            // Create two local variables from user input
+            Console.WriteLine("Enter a number:\n");
+            int first = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n\nEnter another number:\n");
+            int second = Convert.ToInt32(Console.ReadLine());
+
+            // Call the METHOD in the CLASS, passing in user input and specifying the parameters by name
+            Math.integers(first: first, second: second);
+
+            Console.ReadLine();
+        }
+    }
+}

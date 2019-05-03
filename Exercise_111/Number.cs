dfcf92279355ec8f3e9c46a3f64f691e78f55408
Maddy Haldeman <madelyn.haldeman@gmail.com>
2019-05-03 12:06:59 -0700

    Classes and methods

---
+++ Exercise_111/Number.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_111
+{
+    //Create a class. In that class, create three methods, each of which will take one integer parameter in and return an integer.
+    //The methods should do some math operation on the received parameter. Put this class in a separate.cs file in the application.
+    class Number
+    {
+
+        //Create method that generates random number
+        public static int RandomNumber()
+        {
+            Random random = new Random();
+            int r = random.Next(2, 100);
+            return r;
+        }
+        //Create method for adding two numbers
+        public int Addition(int input)
+        {
+            int num2 = RandomNumber();
+            int result = input + num2;
+            Console.WriteLine(input + " + " + num2 + " = " + result);
+            return result;
+        }
+
+        //Create method for multiplying two numbers
+        public int Multiply(int input)
+        {
+            int num2 = RandomNumber();
+            int result = input * num2;
+            Console.WriteLine(input + " * " + num2 + " = " + result);
+            return result;
+        }
+
+        //Create method for dividing two numbers
+        public int Divide(int input)
+        {
+            int num2 = RandomNumber();
+            int result = input / num2;
+            Console.WriteLine(input + " / " + num2 + " = " + result);
+            return result;
+        }
+    }
+}
---
+++ Exercise_111/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_111
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //In the Main() program, ask the user what number they want to do the math operations on.
+            Console.WriteLine("Let's do some math! Enter a number below:\n");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            //Call each method in turn, passing the user input to the method.
+            Number add = new Number();
+            add.Addition(input);
+            add.Multiply(input);
+            add.Divide(input);
+
+            //Display the returned integer to the screen.
+            Console.ReadLine();
+
+        }
+    }
+}

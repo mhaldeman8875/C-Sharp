c23724db0b6348c6cd3241fe365919faa85b7507
Maddy Haldeman <madelyn.haldeman@gmail.com>
2019-05-06 15:14:46 -0700

    Abstract Classes

---
+++ Exercise_124/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_124
+{
+    public class Employee : Person
+    {
+        public override void SayName()
+        {
+            base.SayName();
+        }
+
+        //3. Create another class called Employee and have it inherit from the Person class.
+
+        //4. Implement the SayName() method inside of the Employee class.
+    }
+}
---
+++ Exercise_124/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_124
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //5. Inside the Main() method, instantiate an Employee object with firstName "Sample" and lastName "Student". Call the SayName() method on the object.
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}

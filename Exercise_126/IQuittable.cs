5d1f0a1ccc4308f3bc5e4887618d3770fb8b3296
Maddy Haldeman <madelyn.haldeman@gmail.com>
2019-05-07 12:07:02 -0700

    Polymorphism and interfaces

---
+++ Exercise_126/IQuittable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_126
+{
+    // create an interface called IQuittable
+    interface IQuittable
+    {
+        // create a method called Quit()
+        void Quit();   
+    }
+}
---
+++ Exercise_126/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_126
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            Console.Write("Enter your first name: ");
+            employee.FirstName = Console.ReadLine();
+
+            Console.Write("\nEnter your last name: ");
+            employee.LastName = Console.ReadLine();
+
+            employee.SayName();
+
+            IQuittable quittable = new Employee();
+            quittable.Quit();
+            Console.ReadLine();
+        }
+    }
+}

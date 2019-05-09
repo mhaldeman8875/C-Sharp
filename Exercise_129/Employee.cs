c56481218af24ca8f958c6398d1e7f6a634ad24e
Maddy Haldeman <madelyn.haldeman@gmail.com>
2019-05-09 13:20:25 -0700

    overload == operator

---
+++ Exercise_129/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_129
+{
+    public class Employee
+    {
+        // copy over code from Person class
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int JobID { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName + " " + JobID);
+        }
+        public static bool operator ==(Employee employee, Employee employee2)
+        {
+            if (employee.JobID == employee2.JobID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // == operator also needs to have != operator defined
+        public static bool operator !=(Employee employee, Employee employee2)
+        {
+            if (employee.JobID == employee2.JobID)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ Exercise_129/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_129
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Overload the "==" operator so it checks if two Employee objects are equal by comparing their Id property.
+
+            Console.WriteLine("This program will let you enter some information about two employees and notify you with either 'true' if they have the same job id number and 'false' if not. \n Press ENTER to continue\n");
+            Console.ReadLine();
+
+
+            // have user enter values for first employee
+            Console.Write("Enter an employee's first name: ");
+            string FirstName = Console.ReadLine();
+            Console.Write("Enter an employee's last name: ");
+            string LastName = Console.ReadLine();
+            Console.Write("Enter a number for the employee's job code: ");
+            int JobID = Convert.ToInt32(Console.ReadLine());
+
+            // have user enter values for second employee
+            Console.Write("\n\nEnter an employee's first name: ");
+            string FirstName2 = Console.ReadLine();
+            Console.Write("Enter an employee's last name: ");
+            string LastName2 = Console.ReadLine();
+            Console.Write("Enter a number: ");
+            int JobID2 = Convert.ToInt32(Console.ReadLine());
+
+            // instantiate
+            Employee employee = new Employee() { FirstName = FirstName, LastName = LastName, JobID = JobID };
+            Employee employee2 = new Employee() { FirstName = FirstName2, LastName = LastName2, JobID = JobID2 };
+
+            // print true or false depending on if the ID's match
+            Console.WriteLine("\n" + Convert.ToString(employee == employee2));
+            Console.ReadLine();
+        }
+    }
+}

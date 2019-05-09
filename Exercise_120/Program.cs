34ee56be3cd08d019ccc506c02b0386cbd9be0dd
Maddy Haldeman <madelyn.haldeman@gmail.com>
2019-05-09 10:14:29 -0700

    make corrections in exercise 120 per Garth's instructions

--- Exercise_120/Program.cs
+++ Exercise_120/Program.cs
@@ -11,10 +11,10 @@ namespace Exercise_120
         static void Main(string[] args)
         {
             //inside of the Main method instatiate and init an Employee object with a first name "Sample" and last name "student"
-            Person person = new Person();
-            person.FirstName = "Sample";
-            person.LastName = "Student";
-            person.SayName();
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.SayName();
             Console.ReadLine();
 
         }

9d6d176f2522089ad7cd053677c0c40321d0b67d
Maddy Haldeman <madelyn.haldeman@gmail.com>
2019-05-13 12:07:31 -0700

    Generics

---
+++ Exercise_131_Generics/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Exercise_131_Generics
+{
+    // Make Employee class take a generic type parameter
+    public class Employee<GenParam>
+    {
+        // add property called "things" make it a list and match the generic type of class
+        public List<GenParam> things;
+
+        // Create a loop that prints all of the Things to the Console
+        public void print()
+        {
+            for (int i = 0; i < things.Count; i++)
+            Console.WriteLine(things[i]);
+        }
+    }
+}
---
+++ Exercise_131_Generics/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_131_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate an Employee object with type "string" as its generic parameter.Assign a list of strings as the property value of Things.
+
+            Employee<string> employee = new Employee<string>() { things = new List<string> { "Maddy Haldeman", "Gian Lazzarini", "Maazy Haldeman", "Mike Haldeman", "Eryn Huntington", "Leo Lazzarini" } };
+
+            // Instantiate an Employee object with type "int" as its generic parameter. Assign a list of integers as the property value of Things.
+            Employee<int> employeeInt = new Employee<int>() { things = new List<int>() { 101, 201, 101, 000, 123, 123 } };
+
+            // call the function that runs a loop to print both lists
+            Console.WriteLine("Employees:\n");
+            employee.print();
+            Console.WriteLine("\n\nCorresponding Job IDs:\n");
+            employeeInt.print();
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+

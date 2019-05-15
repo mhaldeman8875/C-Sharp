c9e8eae2626bb4482475e5122b538789e2f7f7a1
Maddy Haldeman <madelyn.haldeman@gmail.com>
2019-05-15 13:23:17 -0700

    Finish Structs exercise

---
+++ Exercise_137_Structs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_137_Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create an object of data type Number and assign an amount to it
+            Number number = new Number() { Amount = 761.82676m };
+
+            // print that number to console
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+    //create a struct called number OUTSIDE OF CLASS (because structs are value types)
+    struct Number
+    {
+        //give the property "AMOUNT" as decimal
+        public decimal Amount { get; set; }
+    }
+
+}

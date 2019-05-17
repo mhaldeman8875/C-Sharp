f3b6ca27afbfa4de32de1325961fafb8c583e99c
Maddy Haldeman <madelyn.haldeman@gmail.com>
2019-05-17 14:29:38 -0700

    IO

---
+++ Exercise_154_FileIO/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Exercise_154_FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ask a user for a number
+            Console.WriteLine("Please enter a number:\n");
+            string input = Console.ReadLine();
+
+            //Log that number to a text file
+            File.WriteAllText(@"C:\Users\madel\Desktop\c-sharp\C-Sharp\Logs\log3.txt", input);
+
+            //. Print the text file back to the user.
+            string read = File.ReadAllText(@"C:\Users\madel\Desktop\c-sharp\C-Sharp\Logs\log3.txt");
+            Console.WriteLine("You just successfully logged the number {0}", read);
+
+            Console.WriteLine("Hit enter to quit");
+            Console.ReadLine();
+        }
+    }
+}

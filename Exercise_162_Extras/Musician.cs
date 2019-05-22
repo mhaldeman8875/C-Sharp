387562c5070ecc3d0b314ee752fe1ad3cd1c82f3
Maddy Haldeman <madelyn.haldeman@gmail.com>
2019-05-22 13:29:53 -0700

    extra features in c#

---
+++ Exercise_162_Extras/Musician.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_162_Extras
+{
+    public class Musician
+    {
+        public string name { get; set; }
+        public string genre { get; set; }
+        public Musician(string Name, string Genre)
+        {
+            name = Name;
+            genre = Genre;
+        }
+        public Musician(string Genre) : this("Lucii", Genre)
+        {
+        }
+        public Musician() : this("Dr. Fresch", "House")
+        {
+        }
+    }
+}
---
+++ Exercise_162_Extras/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_162_Extras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create constant variable
+            const string constant = "This is a constant variable that cannot be changed\n\n";
+            Console.WriteLine(constant);
+
+            // create a variable using the keyword var AND chain two constructors together
+            var musicians = new List<Musician>
+            {
+                new Musician ("Liquid Stranger", "Freeform Bass"),
+                new Musician ("Alien Bass"),
+                new Musician (),
+            };
+
+            foreach (Musician musician in musicians)
+            {
+                Console.WriteLine("Name: {0} \nGenre: {1}\n", musician.name, musician.genre);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
